56d20849dc1e19d9ba389d050fe3fbd219e767df
marcodeiulio <mdeiulio@icloud.com>
2022-03-25 17:57:54 +0100

    - Added fallback route - Edited home

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('guest.home');
-})->name('guest.home');
-
 Auth::routes(['register' => false]);
 
 Route::middleware('auth')
@@ -30,3 +26,7 @@ Route::middleware('auth')
 
         Route::resource('posts', 'PostController');
     });
+
+Route::get('{any?}', function () {
+    return view('guest.home');
+})->where('any', '.*');
