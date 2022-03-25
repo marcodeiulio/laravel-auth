3adc99e9f316730d48e2b906dc209325eb03f517
marcodeiulio <mdeiulio@icloud.com>
2022-03-25 16:53:18 +0100

    - Edited home and welcome views' paths

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('guest.home');
 });
 
-Auth::routes();
+Auth::routes(['register' => false]);
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/admin', 'HomeController@index')->name('admin.home');
